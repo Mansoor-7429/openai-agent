19f3fca3edbfcce5010ebfa34e488e3111f40210
Mansoor-7429 <cpgsfpkksa@gmail.com>
2025-09-15 05:54:43 +0500

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-# app.py
 import os
 from openai import OpenAI
 import gradio as gr
@@ -68,3 +67,4 @@ with gr.Blocks() as demo:
 
 if __name__ == "__main__":
     demo.launch(server_name="0.0.0.0", server_port=int(os.environ.get("PORT", 7860)))
+
